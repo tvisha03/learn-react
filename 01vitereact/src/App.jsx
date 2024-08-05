6714abbdbc0bc5b535cfa69d5ffd9fa7be083810
tvisha03 <tvishakhanna1557@gmail.com>
2024-08-05 14:47:16 +0530

    created custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,12 +4,12 @@ import Chai from "./chai"
 
 
 function App() {
-  
+  const username="User1"
 
   return (
     <>
    <Chai/>
-   <h1>chai aur react</h1>
+   <h1>chai aur react {username}</h1>
    <p>test para</p>
    </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement= React.createElement(
+  'a',
+  {href:'https://reactjs.org',target:'_blank'},
+  'click me to visit react'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <App/>
+    {reactElement}
+    </>
 )
